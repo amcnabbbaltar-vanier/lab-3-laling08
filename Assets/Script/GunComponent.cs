f30e59f58e8a3f129fd1a2b7663d636955f2acdb
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-09-10 11:16:23 -0400

    Task 3
    Modified the GunComponent.cs script and configured the Gun component itself.

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -9,5 +9,30 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
+    void Update()
+    {
+        // TO DO: Add the logic to track player by keeping the input down.
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+        }
+    }
+
+    // This method is responsible for creating a bullet object and applying physical force to it so it shoots forward from the gun.
+    void ShootBullet()
+{
+    // Cretaes a new buller in the game by using the bulletPrefa and placing it at the bulletSpawnPoint's position and rotation.
+    GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+    
+    // Get the RigidBody Component from the bullet.
+    Rigidbody rb = bullet.GetComponent<Rigidbody>();
+    
+
+    float bulletImpulse = bulletMaxImpulse;
+
+    // Apply force to the bullet in a forward direction of the gun.
+    rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+}
+
    
 }
